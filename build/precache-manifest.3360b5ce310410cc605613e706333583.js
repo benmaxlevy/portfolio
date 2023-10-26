e4aa0d60260ae8fa560457bb2f6ba8b47a3b45f5
Ben <general@simplexwebsites.com>
2023-10-26 19:45:03 -0400

    Build!

---
+++ build/precache-manifest.3360b5ce310410cc605613e706333583.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2dbf204087e427bd1a3a9aa8ff4b49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2a2ba6515d21e39358e",
+    "url": "/static/css/2.0c5c7518.chunk.css"
+  },
+  {
+    "revision": "15a5d917739ea8bd05fc",
+    "url": "/static/css/main.af4103f5.chunk.css"
+  },
+  {
+    "revision": "c2a2ba6515d21e39358e",
+    "url": "/static/js/2.697ee5a1.chunk.js"
+  },
+  {
+    "revision": "dd7c7487ad91a0e9c734",
+    "url": "/static/js/3.22d867f5.chunk.js"
+  },
+  {
+    "revision": "15a5d917739ea8bd05fc",
+    "url": "/static/js/main.d6dd2146.chunk.js"
+  },
+  {
+    "revision": "c6e2399f59e1fe5c1086",
+    "url": "/static/js/runtime~main.d8c8ba76.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
